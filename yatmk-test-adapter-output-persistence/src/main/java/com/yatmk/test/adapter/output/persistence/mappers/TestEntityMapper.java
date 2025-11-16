e4aaf184d7143beef9a9a61c7bd125194873a8c0
yassineaitmalek <yassineaitmalek7@gmail.com>
2025-11-16 13:40:48 +0100

    adding mapstruct

---
+++ yatmk-test-adapter-output-persistence/src/main/java/com/yatmk/test/adapter/output/persistence/mappers/TestEntityMapper.java
@@ -0,0 +1,18 @@
+package com.yatmk.test.adapter.output.persistence.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import com.yatmk.test.adapter.output.persistence.models.local.TestEntity;
+import com.yatmk.test.ports.domain.dto.TestCreation;
+import com.yatmk.test.ports.domain.dto.TestDTO;
+
+@Mapper(componentModel = "spring")
+public interface TestEntityMapper {
+
+  public static final TestEntityMapper INSTANCE = Mappers.getMapper(TestEntityMapper.class);
+
+  TestDTO toDTO(TestEntity entity);
+
+  TestEntity toEntity(TestCreation creation);
+}
--- yatmk-test-adapter-output-persistence/src/main/java/com/yatmk/test/adapter/output/persistence/repositories/local/TestEntityRepository.java
+++ yatmk-test-adapter-output-persistence/src/main/java/com/yatmk/test/adapter/output/persistence/repositories/local/TestEntityRepository.java
@@ -1,27 +1,30 @@
 package com.yatmk.test.adapter.output.persistence.repositories.local;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Repository;
+
+import com.yatmk.test.adapter.output.persistence.mappers.TestEntityMapper;
 import com.yatmk.test.adapter.output.persistence.models.local.TestEntity;
 import com.yatmk.test.ports.domain.dto.TestCreation;
 import com.yatmk.test.ports.domain.dto.TestDTO;
 import com.yatmk.test.ports.output.SaveTestPort;
+
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
-import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
 public class TestEntityRepository implements SaveTestPort {
 
-    private  final JpaTestEntityRepository jpaTestEntityRepository;
+    private final JpaTestEntityRepository jpaTestEntityRepository;
 
     private final ModelMapper modelMapper;
 
+    private final TestEntityMapper testEntityMapper;
 
     @Override
     public TestDTO saveTest(TestCreation testCreation) {
         TestEntity testEntity = modelMapper.map(testCreation, TestEntity.class);
         testEntity = jpaTestEntityRepository.save(testEntity);
-        return modelMapper.map(testEntity, TestDTO.class);
-
+        return testEntityMapper.toDTO(testEntity);
     }
 }
