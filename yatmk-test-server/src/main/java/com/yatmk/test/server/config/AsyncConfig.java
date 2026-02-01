06cafdc7a11640c53277f48b0883180d5e99e148
Yassine Ait Malek <75168315+yassineaitmalek@users.noreply.github.com>
2026-02-01 14:42:58 +0100

    formatting

--- yatmk-test-server/src/main/java/com/yatmk/test/server/config/AsyncConfig.java
+++ yatmk-test-server/src/main/java/com/yatmk/test/server/config/AsyncConfig.java
@@ -1,7 +1,6 @@
 package com.yatmk.test.server.config;
 
 import java.util.concurrent.Executor;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -9,14 +8,14 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @Configuration
 public class AsyncConfig {
 
-  @Bean(name = "asyncExecutor")
-  public Executor getAsyncExecutor() {
-    ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-    executor.setCorePoolSize(5);
-    executor.setMaxPoolSize(10);
-    executor.setQueueCapacity(100);
-    executor.setThreadNamePrefix("test-async-");
-    executor.initialize();
-    return executor;
-  }
+    @Bean(name = "asyncExecutor")
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("test-async-");
+        executor.initialize();
+        return executor;
+    }
 }
